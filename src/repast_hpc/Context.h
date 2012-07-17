7402a5dce8224ac96c10f197eba8ac4931e0a98f
John T. Murphy <jtmurphy@anl.gov>
2012-07-17 16:09:44 -0500

    CLEANUP

--- src/repast_hpc/Context.h
+++ src/repast_hpc/Context.h
@@ -780,8 +780,8 @@ Projection<T>* Context<T>::getProjection(const std::string& name) {
 template<typename T>
 T* Context<T>::getAgent(const AgentId& id) {
   AgentMapIterator iter = agents.find(id);
-  if (iter == agents.end())  return 0;
-	return iter->second.get();
+  if (iter == agents.end()) return 0;
+  return iter->second.get();
 }
 
 template<typename T>
