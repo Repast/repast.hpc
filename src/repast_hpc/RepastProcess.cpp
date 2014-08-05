fc997fd1bbbd0bb54dc638b756ad15e722244f20
Nick Collier <srcnick@sourceforge.net>
2014-08-05 13:43:02 -0400

    added check for self-send/recv in RepastProcess constructor

--- src/repast_hpc/RepastProcess.cpp
+++ src/repast_hpc/RepastProcess.cpp
@@ -47,34 +47,34 @@
 #include <boost/unordered_set.hpp>
 #include <boost/serialization/export.hpp>
 
-
 #include "RepastProcess.h"
 #include "logger.h"
 #include "SRManager.h"
 #include "Utilities.h"
 
-BOOST_CLASS_EXPORT_GUID(repast::SpecializedProjectionInfoPacket<double>, "SpecializedProjectionInfoPacket_DOUBLE");
+BOOST_CLASS_EXPORT_GUID(repast::SpecializedProjectionInfoPacket<double>,
+		"SpecializedProjectionInfoPacket_DOUBLE");
 
-BOOST_CLASS_EXPORT_GUID(repast::SpecializedProjectionInfoPacket<int>, "SpecializedProjectionInfoPacket_INT");
-
+BOOST_CLASS_EXPORT_GUID(repast::SpecializedProjectionInfoPacket<int>,
+		"SpecializedProjectionInfoPacket_INT");
 
 using namespace std;
 namespace mpi = boost::mpi;
 
-
 namespace repast {
 
-RepastProcess*            RepastProcess::_instance = 0;
-boost::mpi::communicator* RepastProcess::myWorld   = 0;
+RepastProcess* RepastProcess::_instance = 0;
+boost::mpi::communicator* RepastProcess::myWorld = 0;
 
-RepastProcess::RepastProcess(boost::mpi::communicator* comm):
-  procsToSendProjInfoTo(NULL), procsToRecvProjInfoFrom(NULL),
-  procsToSendAgentStatusInfoTo(NULL), procsToRecvAgentStatusInfoFrom(NULL){
-  if(myWorld == 0) myWorld = new boost::mpi::communicator();
-  world = (comm  != 0 ? comm : myWorld);
-  runner = new ScheduleRunner(world);
-  rank_ = world->rank();
-  worldSize_ = world->size();
+RepastProcess::RepastProcess(boost::mpi::communicator* comm) :
+		procsToSendProjInfoTo(NULL), procsToRecvProjInfoFrom(NULL), procsToSendAgentStatusInfoTo(
+				NULL), procsToRecvAgentStatusInfoFrom(NULL) {
+	if (myWorld == 0)
+		myWorld = new boost::mpi::communicator();
+	world = (comm != 0 ? comm : myWorld);
+	runner = new ScheduleRunner(world);
+	rank_ = world->rank();
+	worldSize_ = world->size();
 
 #ifndef SHARE_AGENTS_BY_SET
 	importer_exporter = new DEFAULT_IMPORTER_EXPORTER_CLASS();
@@ -82,43 +82,52 @@ RepastProcess::RepastProcess(boost::mpi::communicator* comm):
 	importer_exporter = new ImporterExporter_BY_SET();
 #endif
 
-  SpecializedProjectionInfoPacket<double> test1;
-  SpecializedProjectionInfoPacket<double> recvT1;
-  SpecializedProjectionInfoPacket<int>    test2;
-  SpecializedProjectionInfoPacket<int>    recvT2;
-  int topRank = world->size() - 1;
-  int recv = (rank_ > 0        ? rank_ - 1 : topRank);
-  int send = (rank_ < topRank  ? rank_ + 1 : 0);
+	int topRank = world->size() - 1;
+	int recv = (rank_ > 0 ? rank_ - 1 : topRank);
+	int send = (rank_ < topRank ? rank_ + 1 : 0);
 
-  boost::mpi::request requests[4];
-  requests[0] = world->isend(send, 0, test1);
-  requests[1] = world->isend(send, 1, test2);
-  requests[2] = world->irecv(recv, 0, recvT1);
-  requests[3] = world->irecv(recv, 1, recvT2);
-  boost:mpi::wait_all(requests, requests + 4);
+	// "register" the SpecializedProjectionInfoPacket with
+	// boost archive, iff running multiprocess.
+	if (send != recv) {
+		SpecializedProjectionInfoPacket<double> test1;
+		SpecializedProjectionInfoPacket<double> recvT1;
+		SpecializedProjectionInfoPacket<int> test2;
+		SpecializedProjectionInfoPacket<int> recvT2;
+
+		boost::mpi::request requests[4];
+		requests[0] = world->isend(send, 0, test1);
+		requests[1] = world->isend(send, 1, test2);
+		requests[2] = world->irecv(recv, 0, recvT1);
+		requests[3] = world->irecv(recv, 1, recvT2);
+		boost::mpi::wait_all(requests, requests + 4);
+	}
 }
 
-RepastProcess* RepastProcess::init(string propsfile, boost::mpi::communicator* comm, int maxConfigFileSize) {
-  if(myWorld == 0) myWorld = new boost::mpi::communicator();
-  if (_instance == 0){
-    boost::mpi::communicator* tmpWorld = (comm != 0 ? comm : myWorld);
+RepastProcess* RepastProcess::init(string propsfile,
+		boost::mpi::communicator* comm, int maxConfigFileSize) {
+	if (myWorld == 0)
+		myWorld = new boost::mpi::communicator();
+	if (_instance == 0) {
+		boost::mpi::communicator* tmpWorld = (comm != 0 ? comm : myWorld);
 
-    if (propsfile.length() > 0)
-        Log4CL::configure(tmpWorld->rank(), propsfile, tmpWorld, maxConfigFileSize);
-    else
-        Log4CL::configure(tmpWorld->rank());
-    _instance = new RepastProcess(tmpWorld);
-  }
-  return _instance;
-}
-
-RepastProcess* RepastProcess::instance() {
-	if (_instance == 0)	throw Repast_Error_39(); // RepastProcess must be initialized before calling instance
+		if (propsfile.length() > 0)
+			Log4CL::configure(tmpWorld->rank(), propsfile, tmpWorld,
+					maxConfigFileSize);
+		else
+			Log4CL::configure(tmpWorld->rank());
+		_instance = new RepastProcess(tmpWorld);
+	}
 	return _instance;
 }
 
-boost::mpi::communicator* RepastProcess::communicator(){
-  return instance()->getCommunicator();
+RepastProcess* RepastProcess::instance() {
+	if (_instance == 0)
+		throw Repast_Error_39(); // RepastProcess must be initialized before calling instance
+	return _instance;
+}
+
+boost::mpi::communicator* RepastProcess::communicator() {
+	return instance()->getCommunicator();
 }
 
 void RepastProcess::done() {
@@ -138,160 +147,164 @@ void RepastProcess::agentRemoved(const AgentId& id) {
 	importer_exporter->agentRemoved(id);
 }
 
-
 void RepastProcess::moveAgent(const AgentId& id, int process) {
-  AgentId newId(id);
-  newId.currentRank(process);
-  MovedAgentSetType::const_iterator iter = movedAgents.find(newId);
-	if (iter == movedAgents.end()){
-	  importer_exporter->agentMoved(id, process);
-	  movedAgents.insert(newId);
-	}
-  else {
+	AgentId newId(id);
+	newId.currentRank(process);
+	MovedAgentSetType::const_iterator iter = movedAgents.find(newId);
+	if (iter == movedAgents.end()) {
+		importer_exporter->agentMoved(id, process);
+		movedAgents.insert(newId);
+	} else {
 		AgentId other = *iter;
 		if (other.currentRank() != process) {
-			std::cout << rank_ << " : " << other << ", " << id << " trying to move to " << process << std::endl;
-			throw Repast_Error_40<AgentId>(id, rank_, other.currentRank(), process); // Cannot move agent to two different processes during the same iteration
+			std::cout << rank_ << " : " << other << ", " << id
+					<< " trying to move to " << process << std::endl;
+			throw Repast_Error_40<AgentId>(id, rank_, other.currentRank(),
+					process); // Cannot move agent to two different processes during the same iteration
 		}
 	}
 }
 
-
 void RepastProcess::initiateAgentRequest(AgentRequest& request
 #ifdef SHARE_AGENTS_BY_SET
-    , std::string setName, AGENT_IMPORTER_EXPORTER_TYPE setType
+		, std::string setName, AGENT_IMPORTER_EXPORTER_TYPE setType
 #endif
-){
+		) {
 
-  // Record and process the outgoing request for agents from other processes
+	// Record and process the outgoing request for agents from other processes
 #ifndef SHARE_AGENTS_BY_SET
-  importer_exporter->registerOutgoingRequests(request);
+	importer_exporter->registerOutgoingRequests(request);
 #else
-  importer_exporter->registerOutgoingRequests(request, setName, setType);
+	importer_exporter->registerOutgoingRequests(request, setName, setType);
 #endif
 
-  int* countsOfRequests = new int[worldSize_];
-  for(int i = 0; i < worldSize_; ++i) countsOfRequests[i] = 0; // OOPS! This was not included in version 1.0.1 final
-  int maxNumRequests = 0;
+	int* countsOfRequests = new int[worldSize_];
+	for (int i = 0; i < worldSize_; ++i)
+		countsOfRequests[i] = 0; // OOPS! This was not included in version 1.0.1 final
+	int maxNumRequests = 0;
 
-  const vector<AgentId>& requestedAgents = request.requestedAgents();
-  const vector<AgentId>& cancellations   = request.cancellations();
+	const vector<AgentId>& requestedAgents = request.requestedAgents();
+	const vector<AgentId>& cancellations = request.cancellations();
 
-  vector<AgentId>::const_iterator agentId;
-  vector<AgentId>::const_iterator agentIdEnd;
+	vector<AgentId>::const_iterator agentId;
+	vector<AgentId>::const_iterator agentIdEnd;
 
-  agentIdEnd = requestedAgents.end();
-  for(agentId = requestedAgents.begin(); agentId != agentIdEnd; agentId++){
-    int target = agentId->currentRank();
-    countsOfRequests[target]++;
-    if(countsOfRequests[target] > maxNumRequests) maxNumRequests++;
-  }
+	agentIdEnd = requestedAgents.end();
+	for (agentId = requestedAgents.begin(); agentId != agentIdEnd; agentId++) {
+		int target = agentId->currentRank();
+		countsOfRequests[target]++;
+		if (countsOfRequests[target] > maxNumRequests)
+			maxNumRequests++;
+	}
 
-  agentIdEnd = cancellations.end();
-  for(agentId = cancellations.begin(); agentId != agentIdEnd; agentId++){
-    int target = agentId->currentRank();
-    countsOfRequests[target]++;
-    if(countsOfRequests[target] > maxNumRequests) maxNumRequests++;
-  }
+	agentIdEnd = cancellations.end();
+	for (agentId = cancellations.begin(); agentId != agentIdEnd; agentId++) {
+		int target = agentId->currentRank();
+		countsOfRequests[target]++;
+		if (countsOfRequests[target] > maxNumRequests)
+			maxNumRequests++;
+	}
 
-  int maxGlobalNumRequests = 0;
+	int maxGlobalNumRequests = 0;
 
-  MPI_Allreduce(&maxNumRequests, &maxGlobalNumRequests, 1, MPI_INT, MPI_MAX, *world);
+	MPI_Allreduce(&maxNumRequests, &maxGlobalNumRequests, 1, MPI_INT, MPI_MAX,
+			*world);
 
+	// Reset and reuse the values from countsOfRequests
+	for (int i = 0; i < worldSize_; i++)
+		countsOfRequests[i] = 0;
 
-  // Reset and reuse the values from countsOfRequests
-  for(int i = 0; i < worldSize_; i++) countsOfRequests[i] = 0;
+	// Create an array for the actual send; note 'padding' for extra values
+	// Size of an individual element is 3 ints; don't need to send 'current rank'
+	int dataElementSize = (maxGlobalNumRequests + 2) * 3;
+	int dataSize = dataElementSize * worldSize_;
+	int* data = new int[dataSize];
+	int* rec = new int[dataSize];
 
-  // Create an array for the actual send; note 'padding' for extra values
-  // Size of an individual element is 3 ints; don't need to send 'current rank'
-  int dataElementSize = (maxGlobalNumRequests + 2) * 3;
-  int dataSize = dataElementSize * worldSize_;
-  int* data = new int[dataSize];
-  int* rec  = new int[dataSize];
+	// Initialize
+	for (int i = 0; i < dataSize; i++)
+		data[i] = -1; // Can't be zero
 
-  // Initialize
-  for(int i = 0; i < dataSize; i++) data[i] = -1; // Can't be zero
+	// Now move the data into the send buffer:
+	agentIdEnd = requestedAgents.end();
+	for (agentId = requestedAgents.begin(); agentId != agentIdEnd; agentId++) {
+		int target = agentId->currentRank();
+		int pos = target * dataElementSize + countsOfRequests[target] * 3;
+		data[pos] = agentId->id();
+		data[pos + 1] = agentId->startingRank();
+		data[pos + 2] = agentId->agentType();
+		countsOfRequests[target]++;
+	}
 
-  // Now move the data into the send buffer:
-  agentIdEnd = requestedAgents.end();
-  for(agentId = requestedAgents.begin(); agentId != agentIdEnd; agentId++){
-    int target = agentId->currentRank();
-    int pos = target * dataElementSize + countsOfRequests[target] * 3;
-    data[pos]   = agentId->id();
-    data[pos+1] = agentId->startingRank();
-    data[pos+2] = agentId->agentType();
-    countsOfRequests[target]++;
-  }
+	for (int i = 0; i < worldSize_; i++)
+		countsOfRequests[i]++; // Skip one entry to mark boundary between requests and cancellations
 
-  for(int i = 0; i < worldSize_; i++) countsOfRequests[i]++; // Skip one entry to mark boundary between requests and cancellations
+	agentIdEnd = cancellations.end();
+	for (agentId = cancellations.begin(); agentId != agentIdEnd; agentId++) {
+		int target = agentId->currentRank();
+		int pos = target * dataElementSize + countsOfRequests[target] * 3;
+		data[pos] = agentId->id();
+		data[pos + 1] = agentId->startingRank();
+		data[pos + 2] = agentId->agentType();
+		countsOfRequests[target]++;
+	}
 
-  agentIdEnd = cancellations.end();
-  for(agentId = cancellations.begin(); agentId != agentIdEnd; agentId++){
-    int target = agentId->currentRank();
-    int pos = target * dataElementSize + countsOfRequests[target] * 3;
-    data[pos]   = agentId->id();
-    data[pos+1] = agentId->startingRank();
-    data[pos+2] = agentId->agentType();
-    countsOfRequests[target]++;
-  }
+	// Exchange data
+	MPI_Alltoall(data, dataElementSize, MPI_INT, rec, dataElementSize, MPI_INT,
+			*world);
+	delete data; // Done with this...
 
-  // Exchange data
-  MPI_Alltoall(data, dataElementSize, MPI_INT, rec, dataElementSize, MPI_INT, *world);
-  delete data; // Done with this...
+	// Now re-package the received data as the vector<AgentRequest> that is needed
 
-  // Now re-package the received data as the vector<AgentRequest> that is needed
+	vector<AgentRequest> reqsRecd;
+	for (int i = 0; i < worldSize_; i++) {
+		if (i != rank_) { // This isn't necessary as long as a process doesn't request agents from itself (!); this acts as an error trap if this happens
+			int index = i * dataElementSize;
+			// If there were no requests OR cancellations sent, skip
+			if (!((rec[index] == -1) && rec[index + 3] == -1)) {
+				// Create the agent request
+				AgentRequest req(i, rank_);
+				// Add all the requests
+				while (rec[index] != -1) {
+					AgentId id(rec[index], rec[index + 1], rec[index + 2]);
+					id.currentRank(rank_);
+					req.addRequest(id);
+					index += 3;
+				}
+				index += 3;
+				while (rec[index] != -1) {
+					AgentId id(rec[index], rec[index + 1], rec[index + 2]);
+					id.currentRank(rank_);
+					req.addCancellation(id);
+					index += 3;
+				}
+				// Add (a copy of) the request to the vector
+				reqsRecd.push_back(req);
+			}
+		}
+	}
 
-  vector<AgentRequest> reqsRecd;
-  for(int i = 0; i < worldSize_; i++){
-    if(i != rank_){  // This isn't necessary as long as a process doesn't request agents from itself (!); this acts as an error trap if this happens
-      int index = i * dataElementSize;
-      // If there were no requests OR cancellations sent, skip
-      if(!((rec[index] == -1) && rec[index + 3] == -1)){
-        // Create the agent request
-        AgentRequest req(i, rank_);
-        // Add all the requests
-        while(rec[index] != -1){
-          AgentId id(rec[index], rec[index + 1], rec[index + 2]);
-          id.currentRank(rank_);
-          req.addRequest(id);
-          index += 3;
-        }
-        index += 3;
-        while(rec[index] != -1){
-          AgentId id(rec[index], rec[index + 1], rec[index + 2]);
-          id.currentRank(rank_);
-          req.addCancellation(id);
-          index += 3;
-        }
-        // Add (a copy of) the request to the vector
-        reqsRecd.push_back(req);
-      }
-    }
-  }
+	delete countsOfRequests;
+	delete rec;
 
-  delete countsOfRequests;
-  delete rec;
-
-
-  // Set up export of agents requested by other processes
+	// Set up export of agents requested by other processes
 #ifndef SHARE_AGENTS_BY_SET
-  importer_exporter->registerIncomingRequests(reqsRecd);
+	importer_exporter->registerIncomingRequests(reqsRecd);
 #else
-  importer_exporter->registerIncomingRequests(reqsRecd, setName);
+	importer_exporter->registerIncomingRequests(reqsRecd, setName);
 #endif
 
 }
 
-
 RepastProcess::~RepastProcess() {
-  delete runner;
-  delete importer_exporter;
+	delete runner;
+	delete importer_exporter;
 
-  delete procsToSendProjInfoTo;
-  delete procsToRecvProjInfoFrom;
+	delete procsToSendProjInfoTo;
+	delete procsToRecvProjInfoFrom;
 
-  delete procsToSendAgentStatusInfoTo;
-  delete procsToRecvAgentStatusInfoFrom;
+	delete procsToSendAgentStatusInfoTo;
+	delete procsToRecvAgentStatusInfoFrom;
 
 	_instance = 0;
 }
