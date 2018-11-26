b2dcab29d8c7a3c19eb3c2f5ef40c025d87fd298
Nick Collier <nick.collier@gmail.com>
2018-11-26 14:21:42 -0500

    Updated module.mk

--- src/repast_hpc/module.mk
+++ src/repast_hpc/module.mk
@@ -1,31 +1,35 @@
 SOURCES = AgentId.cpp \
-NCDataSetBuilder.cpp \
-SharedNetwork.cpp \
 AgentImporterExporter.cpp \
-NCDataSet.cpp \
-spatial_math.cpp \
 AgentRequest.cpp \
-NetworkBuilder.cpp \
-SRManager.cpp \
 AgentStatus.cpp \
-Properties.cpp \
-SVDataSetBuilder.cpp \
+CartesianTopology.cpp \
 Graph.cpp \
-Random.cpp \
-SVDataSet.cpp \
 GridComponents.cpp \
-RepastErrors.cpp \
-Utilities.cpp \
 GridDimensions.cpp \
-RepastProcess.cpp \
-ValueLayer.cpp \
 initialize_random.cpp \
-Schedule.cpp \
-Variable.cpp \
 io.cpp \
-SharedBaseGrid.cpp \
 logger.cpp \
-SharedContext.cpp
+NCDataSet.cpp \
+NCDataSetBuilder.cpp \
+NetworkBuilder.cpp \
+Properties.cpp \
+Random.cpp \
+RelativeLocation.cpp \
+RepastErrors.cpp \
+RepastProcess.cpp \
+Schedule.cpp \
+SharedBaseGrid.cpp \
+SharedContext.cpp \
+spatial_math.cpp \
+SRManager.cpp \
+SVDataSet.cpp \
+SVDataSetBuilder.cpp \
+Utilities.cpp \
+ValueLayer.cpp \
+ValueLayerND.cpp \
+Variable.cpp \
+ValueLayer.cpp \
+Variable.cpp 
 
 local_dir := repast_hpc
 local_src := $(addprefix $(local_dir)/, $(SOURCES))
