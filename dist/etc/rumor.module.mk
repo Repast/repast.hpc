a7a75a65d45e0be783906ff34db2729693c9db9d
removedporn <86824510+removedporn@users.noreply.github.com>
2023-08-09 20:21:06 +0800

    Delete dist directory

--- dist/etc/rumor.module.mk
+++
@@ -1,4 +0,0 @@
-DIR := src/rumor
-
-RUMOR_SRC += $(DIR)/main.cpp
-RUMOR_SRC += $(DIR)/RumorModel.cpp
--- dist/etc/zombie.module.mk
+++
@@ -1,7 +0,0 @@
-DIR := src/zombie
-
-ZOMBIE_SRC += $(DIR)/main.cpp
-ZOMBIE_SRC += $(DIR)/Zombie.cpp
-ZOMBIE_SRC += $(DIR)/Human.cpp
-ZOMBIE_SRC += $(DIR)/ZombieObserver.cpp
-ZOMBIE_SRC += $(DIR)/InfectionSum.cpp
